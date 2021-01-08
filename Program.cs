a2e1c9598e6f318643a922a1e1d096a0fffc9e7f
SanduniNimanthika <thslsanduni1997@gmail.com>
2021-01-08 10:52:29 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+	public class Program
+	{
+		private static Program instance;
+		private Program()
+		{
+
+		}
+		public static Program Instance
+		{
+			get
+			{
+				if (instance == null)
+				{
+					instance = new Program();
+				}
+				return instance;
+			}
+		}
+	}
+		}
---
+++ TestProgrammer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class TestProgrammer
+    {
+        static void Main()
+        {
+            Program obj1 = Program.Instance;
+            Program obj2 = Program.Instance;
+            Console.WriteLine(obj1.GetHashCode());
+            Console.WriteLine(obj2.GetHashCode());
+
+            Console.WriteLine("Not singleton");
+            Test obj3 = new Test();
+            Test obj4 = new Test();
+            Console.WriteLine(obj3.GetHashCode());
+            Console.WriteLine(obj4.GetHashCode());
+        }
+    }
+
+    class Test
+    {
+
+    }
+}
